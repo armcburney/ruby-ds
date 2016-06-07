4fec21f21acf4eeca12995caa84c74576106b2e5
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-06-06 20:01:11 -0400

    Old commit.

--- astro_4.cr
+++ astro_4.cr
@@ -1,3 +1,6 @@
+# Constants
+h = 6.63e-34
+c = 3e8
 
 # Problem 5.47
 
@@ -9,10 +12,13 @@ puts getWatts(2500*4184,24*60*60)
 
 # Problem 5.51
 
-def calcWavelength(e)
-  h = 6.63e-34
-  c = 3e8
+def calcWavelength(e,h,c)
   return (h*c)/e
 end
 
-puts calcWavelength(1.1e4*1.6e-19)
+def calcFreq(e,h)
+  return e/h
+end
+
+puts calcWavelength(1.1e4*1.6e-19,h,c)
+puts calcFreq(1.1e4*1.6e-19,h)
