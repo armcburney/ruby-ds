1f9d6f75261e69c838fe3aaad143c731bee0bdd9
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-10-17 08:26:50 -0400

    Updated routes.

--- app.py
+++ app.py
@@ -6,11 +6,13 @@
     Coded by: Andrew McBurney
 """
 
+import glob, os
 from flask import Flask
 from flask import render_template as render
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
 
 @app.route('/')
 def index():
@@ -21,19 +23,40 @@ def index():
 
 
 @app.route('/directors')
-def directors():
+def list_directors():
     """
-        List of engsoc directors.
+        Renders the list of engsoc directors all EngSoc directors
     """
     return render('directors.jade')
 
 
 @app.route('/directors/<dirname>')
-def director_info(dirname):
+def show_director(dirname):
     """
-        Listing for each engsoc director.
+        Listing for each engsoc director. Shows things such as name, email,
+        directorships they're listed for.
     """
+    for file in os.listdir("/static/private/directors"):
+        if file.endswith(".txt"):
+            print(file)
+
     return render('director_name.jade', name=dirname)
+
+
+@app.route('/directorships')
+def list_directorships():
+    """
+        Lists all directorships, and the directors that belong to them.
+    """
+    return render('directorships.jade')
+
+
+@app.route('/directorships/<directorship>')
+def show_directorship():
+    """
+        Lists all directorships, and the directors that belong to them.
+    """
+    return render('directorship.jade', name=directorship)
 
 
 if __name__ == "__main__":
